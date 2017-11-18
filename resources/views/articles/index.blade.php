4c0927052715371452d6c6d603d7716db4aad437
simontuo <simontuo666@gmail.com>
2017-11-18 22:40:38 +0800

    remake article index

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -5,20 +5,7 @@
 
 </div>
 <div class="col-md-10">
-    <div class="row">
-        @foreach($articles as $article)
-            <div class="col-md-3">
-                <a href="{{ route('articles.show', [ 'id' => $article->id ]) }}">
-                    <article-card
-                    image="{{ $article->user->avatar }}"
-                    title="{{ $article->title }}"
-                    ></article-card>
-                </a>
-            </div>
-        @endforeach
-
-
-    </div>
+    <article-list></article-list>
 </div>
 
 @endsection
