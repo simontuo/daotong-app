04f5afd925fd87bbd23673c0ccce06bed3001d54
simontuo <simontuo666@gmail.com>
2017-12-26 21:54:47 +0800

    fix footer css

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="footer">
+<footer class="footer mdui-m-y-2">
     <p>Developed By <a href="https://github.com/Seaony" target="_blank">Simontuo</a> <Icon type="heart"></Icon> Fjy</p>
 </footer>
